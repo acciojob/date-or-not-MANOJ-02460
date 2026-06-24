96849c5adc82a13f8bbab6a94c03f1a703cd3b4e
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-06-24 14:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var isDate = function (input) {
   //   write your code here
-};
 
+	if(input === new Date()){
+		return true
+	}
+	return false
+	
+};
+  
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
